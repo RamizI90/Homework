b9c92a47a64f320f3d4dbc1ffef0f4124bff413a
Рамиз <ramiz.ilyazov@mail.ru>
2022-08-14 16:47:01 +0300

    Ex 13

---
+++ Program13.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или 
+// сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите трехзначное число: ");
+string? numberstring = Console.ReadLine();
+if (numberstring!.Length > 2)
+{
+    Console.WriteLine(numberstring[2]);
+}
+
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+
+
